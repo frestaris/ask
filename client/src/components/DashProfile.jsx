089c51ea3c04ea7e4586b0d7563166b9366a14fb
Aris <frestaris@gmail.com>
2024-11-08 18:55:52 +1000

    complete user image upload functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,5 +1,150 @@
+import { useEffect, useState, useRef } from "react";
+import { useSelector } from "react-redux";
+
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
 function DashProfile() {
-  return <div>DashProfile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const filePickerRef = useRef();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+  const uploadImage = async () => {
+    setImageFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadError(
+          "Could not upload image (File must be less than 2MB)"
+        );
+        setImageFileUploadProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+          setImageFileUrl(downloadUrl);
+          setImageFileUploadProgress(null);
+        });
+      }
+    );
+  };
+
+  return (
+    <div className="container mx-auto p-3 w-100" style={{ maxWidth: "30rem" }}>
+      <h1 className="my-4 text-center fw-semibold display-5">Profile</h1>
+      <form className="d-flex flex-column gap-3">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="position-relative mx-auto rounded-circle overflow-hidden shadow"
+          style={{ width: "8rem", height: "8rem", cursor: "pointer" }}
+          onClick={() => filePickerRef.current.click()}
+        >
+          {imageFileUploadProgress && (
+            <CircularProgressbar
+              value={imageFileUploadProgress || 0}
+              text={`${imageFileUploadProgress}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(62, 152, 199, ${
+                    imageFileUploadProgress / 100
+                  })`,
+                },
+              }}
+            />
+          )}
+          <img
+            src={imageFileUrl || currentUser.profilePicture}
+            alt="user"
+            className={`rounded-circle w-100 h-100 object-cover ${
+              imageFileUploadProgress &&
+              imageFileUploadProgress < 100 &&
+              "opacity-75"
+            }`}
+            style={{ border: "5px solid orange" }}
+          />
+        </div>
+        {imageFileUploadError && (
+          <div className="alert alert-danger mt-2" role="alert">
+            {imageFileUploadError}
+          </div>
+        )}
+        <input
+          type="text"
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser.username}
+          className="form-control"
+        />
+        <input
+          type="email"
+          id="email"
+          placeholder="email"
+          defaultValue={currentUser.email}
+          className="form-control"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="******"
+          className="form-control"
+        />
+        <button type="submit" className="btn btn-outline-warning mt-2">
+          Update
+        </button>
+      </form>
+      <div className="d-flex justify-content-between mt-4 text-danger">
+        <span className="cursor-pointer">Delete Account</span>
+        <span className="cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
 }
 
 export default DashProfile;
