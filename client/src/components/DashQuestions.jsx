36d07adb7820a8ef52f0e47c44e55393f60c602d
Aris <frestaris@gmail.com>
2024-11-09 13:25:57 +1000

    add show more functionality to the questions results of the dashboard

--- client/src/components/DashQuestions.jsx
+++ client/src/components/DashQuestions.jsx
@@ -8,24 +8,45 @@ function DashQuestions() {
   const { currentUser } = useSelector((state) => state.user);
   const [userQuestions, setUserQuestions] = useState([]);
   const { theme } = useSelector((state) => state.theme);
+  const [showMore, setShowMore] = useState(true);
 
   useEffect(() => {
     const fetchQuestions = async () => {
       try {
-        const res = await fetch(
-          `/api/question/getquestions?userId=${currentUser._id}`
-        );
+        const res = await fetch(`/api/question/getquestions`);
         const data = await res.json();
+
         if (res.ok && data.questions) {
           setUserQuestions(data.questions);
+          if (data.questions.length < 9) {
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log("Error fetching questions:", error);
       }
     };
 
-    if (currentUser.isAdmin) fetchQuestions();
-  }, [currentUser._id]);
+    fetchQuestions();
+  }, [currentUser._id, currentUser.isAdmin]);
+
+  const handleShowMore = async () => {
+    const startIndex = userQuestions.length;
+    try {
+      const res = await fetch(
+        `/api/question/getquestions?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserQuestions((prev) => [...prev, ...data.questions]);
+        if (data.questions.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div
@@ -36,70 +57,79 @@ function DashQuestions() {
       }}
     >
       {currentUser.isAdmin && userQuestions.length > 0 ? (
-        <Table
-          striped
-          bordered
-          hover
-          responsive
-          variant={theme === "dark" ? "dark" : "light"}
-        >
-          <thead>
-            <tr>
-              <th>Date Updated</th>
-              <th>Question Image</th>
-              <th>Question Title</th>
-              <th>Category</th>
-              <th>Delete</th>
-              <th>Edit</th>
-            </tr>
-          </thead>
-          <tbody>
-            {userQuestions.map((question) => (
-              <tr key={question._id}>
-                <td>{new Date(question.updatedAt).toLocaleDateString()}</td>
-                <td>
-                  <Link to={`/question${question.slug}`}>
-                    <img
-                      src={question.image}
-                      alt={question.title}
-                      style={{
-                        width: "100px",
-                        height: "60px",
-                        objectFit: "cover",
-                        backgroundColor: "#ccc",
-                      }}
-                    />
-                  </Link>
-                </td>
-                <td>
-                  <Link
-                    className={theme === "dark" ? "text-white" : "text-dark"}
-                    to={`/question${question.slug}`}
-                  >
-                    {question.title}
-                  </Link>
-                </td>
-                <td>{question.category}</td>
-                <td className="text-center">
-                  <Button
-                    variant={theme === "dark" ? "danger" : "outline-danger"}
-                    onClick={() => console.log("Delete", question._id)}
-                  >
-                    <RiDeleteBin7Fill />
-                  </Button>
-                </td>
-                <td className="text-center">
-                  <Link
-                    variant={theme === "dark" ? "info" : "outline-info"}
-                    onClick={() => console.log("Edit", question._id)}
-                  >
-                    <RiEdit2Fill />
-                  </Link>
-                </td>
+        <>
+          <Table
+            striped
+            bordered
+            hover
+            responsive
+            variant={theme === "dark" ? "dark" : "light"}
+          >
+            <thead>
+              <tr>
+                <th>Date Updated</th>
+                <th>Question Image</th>
+                <th>Question Title</th>
+                <th>Category</th>
+                <th>Delete</th>
+                <th>Edit</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {userQuestions.map((question) => (
+                <tr key={question._id}>
+                  <td>{new Date(question.updatedAt).toLocaleDateString()}</td>
+                  <td>
+                    <Link to={`/question${question.slug}`}>
+                      <img
+                        src={question.image}
+                        alt={question.title}
+                        style={{
+                          width: "100px",
+                          height: "60px",
+                          objectFit: "cover",
+                          backgroundColor: "#ccc",
+                        }}
+                      />
+                    </Link>
+                  </td>
+                  <td>
+                    <Link
+                      className={theme === "dark" ? "text-white" : "text-dark"}
+                      to={`/question${question.slug}`}
+                    >
+                      {question.title}
+                    </Link>
+                  </td>
+                  <td>{question.category}</td>
+                  <td className="text-center">
+                    <Button
+                      variant={theme === "dark" ? "danger" : "outline-danger"}
+                      onClick={() => console.log("Delete", question._id)}
+                    >
+                      <RiDeleteBin7Fill />
+                    </Button>
+                  </td>
+                  <td className="text-center">
+                    <Link
+                      variant={theme === "dark" ? "info" : "outline-info"}
+                      onClick={() => console.log("Edit", question._id)}
+                    >
+                      <RiEdit2Fill />
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          {showMore && (
+            <div className="text-center">
+              <Button variant="outline-warning" onClick={handleShowMore}>
+                Show More
+              </Button>
+            </div>
+          )}
+        </>
       ) : (
         <p>You have no questions yet!</p>
       )}
