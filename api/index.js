49066dcfac512dfc2113bd6fc2d1eff8bd3b4b91
Aris <frestaris@gmail.com>
2024-12-14 08:31:00 +1000

    removed the dist from index.js backend

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ import authRoutes from "./routes/auth.route.js";
 import questionRoutes from "./routes/question.route.js";
 import cookieParser from "cookie-parser";
 import commentRoutes from "./routes/comment.route.js";
-import path from "path";
 import cors from "cors";
 
 dotenv.config();
@@ -22,8 +21,6 @@ mongoose
   .catch((error) => {
     console.error("MongoDB connection error:", error);
   });
-
-const __dirname = path.resolve();
 
 const app = express();
 
@@ -42,11 +39,6 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/question", questionRoutes);
 app.use("/api/comment", commentRoutes);
-
-app.use(express.static(path.join(__dirname, "client/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
