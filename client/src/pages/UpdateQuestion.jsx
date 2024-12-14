847084cc5904cd2ac4718d26b8746893ca4e11b4
Aris <frestaris@gmail.com>
2024-12-14 10:03:27 +1000

    updated question

--- client/src/pages/UpdateQuestion.jsx
+++ client/src/pages/UpdateQuestion.jsx
@@ -103,6 +103,7 @@ function UpdateQuestion() {
           method: "PUT",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(formData),
+          credentials: "include",
         }
       );
       const data = await res.json();
